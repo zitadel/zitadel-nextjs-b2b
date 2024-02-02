0a0e76e5cb730ab491861192b4d397d1b102e94e
peintnermax <max@caos.ch>
2024-02-02 16:19:26 +0100

    auth api, refresh token

--- components/AuthCheck.tsx
+++ components/AuthCheck.tsx
@@ -9,7 +9,7 @@ export default function AuthCheck(props: any) {
           <div className="flex flex-col items-center">
             <p className="mb-4">You must be signed in!</p>
             <button
-              onClick={() => signIn("zitadel" as any, { callbackUrl: "/" })}
+              onClick={() => signIn('zitadel', { callbackUrl: '/' })}
               className="whitespace-nowrap py-2 px-4 md:mr-0 shadow rounded-md bg-black dark:text-white dark:bg-zitadelaccent-500 dark:hover:bg-zitadelaccent-400 text-white text-sm hidden md:block font-normal"
             >
               Login
--- pages/api/auth/[...nextauth].tsx
+++ pages/api/auth/[...nextauth].tsx
@@ -1,74 +1,91 @@
+import { NextApiRequest, NextApiResponse } from 'next';
 import NextAuth from 'next-auth';
+import { JWT } from 'next-auth/jwt';
+import ZitadelProvider from 'next-auth/providers/zitadel';
+import { Issuer } from 'openid-client';
 
-export default NextAuth({
-  callbacks: {
-    async jwt({ token, user, account, profile, isNewUser }) {
-      if (profile?.sub) {
-        token.sub = profile.sub;
-      }
-      if (account?.access_token) {
-        token.accessToken = account.access_token;
-      }
-      token.idToken ??= account?.id_token;
-      if (typeof user !== typeof undefined) {
-        token.user = user;
-      }
-      return token;
+const authconfig = async (req: NextApiRequest, res: NextApiResponse) =>
+  NextAuth(req, res, {
+    pages: {
+      signIn: '/signin',
     },
-    async session({ session, token: { id, sub, user, error: tokenError, accessToken, idToken } }) {
-      session.accessToken = accessToken;
-      session.idToken = idToken as string;
-      session.user = {
-        id: user?.id,
-        email: user?.email,
-        image: user?.image,
-        name: user?.name,
-        loginName: user?.loginName,
-        orgName: user?.organization.name,
-      };
-      session.clientId = process.env.ZITADEL_CLIENT_ID;
-      session.error = tokenError;
+    callbacks: {
+      async jwt({ token, user, account }) {
+        if (req.url?.startsWith('/api/auth/session?orgName=')) {
+          if (req.query.orgName && typeof req.query.orgName === 'string' && token.user && token.user.organization) {
+            token.user.organization.name = req.query.orgName;
+          }
+        }
 
-      return session;
-    },
-  },
-  debug: true,
-  providers: [
-    {
-      id: 'zitadel',
-      name: 'zitadel',
-      type: 'oauth',
-      version: '2',
-      wellKnown: process.env.NEXT_PUBLIC_ZITADEL_ISSUER,
-      authorization: {
-        params: {
-          scope: `openid email profile urn:zitadel:iam:org:project:id:zitadel:aud`,
-        },
+        token.user ??= user;
+        token.accessToken ??= account?.access_token;
+        token.refreshToken ??= account?.refresh_token;
+        token.expiresAt ??= (account?.expires_at ?? 0) * 1000;
+        token.error = undefined;
+        // Return previous token if the access token has not expired yet
+        if (Date.now() < (token.expiresAt as number)) {
+          return token;
+        }
+
+        // Access token has expired, try to update it
+        return refreshAccessToken(token);
       },
-      idToken: true,
-      checks: ['pkce', 'state'],
-      client: {
-        token_endpoint_auth_method: 'none',
-      },
-      async profile(profile) {
-        return {
-          id: profile.sub,
-          name: profile.name,
-          firstName: profile.given_name,
-          lastName: profile.family_name,
-          email: profile.email,
-          loginName: profile.preferred_username,
-          image: profile.picture,
-          roles: profile['urn:zitadel:iam:org:project:roles'],
-          organization: {
-            id: profile['urn:zitadel:iam:user:resourceowner:id'],
-            name: profile['urn:zitadel:iam:user:resourceowner:name'],
-            primaryDomain: profile['urn:zitadel:iam:user:resourceowner:primary_domain'],
-          },
+      async session({ session, token: { user, error: tokenError, accessToken, idToken } }) {
+        session.accessToken = accessToken;
+        session.idToken = idToken as string;
+        session.user = {
+          id: user?.id,
+          email: user?.email,
+          image: user?.image,
+          name: user?.name,
+          loginName: user?.loginName,
+          orgName: user?.organization?.name,
         };
-      },
+        session.clientId = process.env.ZITADEL_CLIENT_ID;
+        session.error = tokenError;
 
-      clientId: process.env.ZITADEL_CLIENT_ID,
+        return session;
+      },
     },
-  ],
-});
+    debug: true,
+    providers: [
+      ZitadelProvider({
+        issuer: process.env.ZITADEL_ISSUER,
+        clientId: process.env.ZITADEL_CLIENT_ID,
+        clientSecret: process.env.ZITADEL_CLIENT_SECRET,
+      }),
+    ],
+  });
+
+export default authconfig;
+
+/**
+ * Takes a token, and returns a new token with updated
+ * `accessToken` and `accessTokenExpires`. If an error occurs,
+ * returns the old token and an error property
+ */
+async function refreshAccessToken(token: JWT): Promise<JWT> {
+  try {
+    const issuer = await Issuer.discover(process.env.ZITADEL_ISSUER ?? '');
+    const client = new issuer.Client({
+      client_id: process.env.ZITADEL_CLIENT_ID || '',
+      token_endpoint_auth_method: 'none',
+    });
+
+    const { refresh_token, access_token, expires_at } = await client.refresh(token.refreshToken as string);
+
+    return {
+      ...token,
+      accessToken: access_token,
+      expiresAt: (expires_at ?? 0) * 1000,
+      refreshToken: refresh_token, // Fall back to old refresh token
+    };
+  } catch (error) {
+    console.error('Error during refreshAccessToken', error);
+
+    return {
+      ...token,
+      error: 'RefreshAccessTokenError',
+    };
+  }
+}
